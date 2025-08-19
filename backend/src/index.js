1e6689a045ecd9d26ad90068db6822bcaab974df
Santoshb22 <sb289848@gmail.com>
2025-08-19 09:43:01 +0530

    setup mongodb connection and middlewares: express.json urlencoded static cors cookieparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,19 @@
-const app = require("./app");
+import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        })
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
