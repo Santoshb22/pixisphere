762ba0d20b6e12c049f9ca591af459194dc52b6c
Santoshb22 <sb289848@gmail.com>
2025-08-19 09:49:27 +0530

    add user model hashpassword generate jwt token and photographer model

---
+++ backend/src/models/photographer.model.js
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+
+const photographerSchema = new mongoose.Schema(
+    {
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+
+        name: {
+            type: String,
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+        },
+
+        contactNumber: {
+            type: Number,
+        },
+
+        bio: {
+            type: String,
+            trim: true,
+        },
+
+        thumbnail: {
+            type: String,
+        },
+
+        portfolio: [
+            {
+                image: {
+                    type: String, //cloudinary url
+                },
+
+                title: {
+                    type: String,
+                    minlength: [3, 'Title must be at least 3 characters long'],
+                    maxlength: [50, 'Title cannot exceed 50 characters'],     
+                    trim: true
+                }
+            }
+        ],
+
+        likedby: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+
+        ratings: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User"
+                },
+                value: {
+                    type: Number,
+                    min: 1,
+                    max: 5
+                }
+            }
+        ],
+
+        isPublic: {
+            type: Boolean,
+            default: false,
+        },
+
+        paymentPlan: {
+            type: String,
+            enum: ["none", "monthly", "yearly"],
+            default: "none",
+        }
+    }, 
+    
+    {
+        timestamps: true,
+    }
+)
+
+const Photographer = mongoose.model("Photographer", photographerSchema);
+
+export default Photographer;
